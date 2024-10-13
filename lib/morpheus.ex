dacdce0252aab94b75a0dfdeba2663c70cc60ac9
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-10-13 16:41:48 +0530

    feat: add conversion functions
    - Implemented `camel_to_snake/1` to convert camelCase strings to snake_case.
    - Implemented `snake_to_camel/1` to convert snake_case strings to camelCase.
    - Added private function `downcase_first/1` to lowercase the first letter of a string for proper camelCase conversion.

--- lib/morpheus.ex
+++ lib/morpheus.ex
@@ -1,18 +1,21 @@
 defmodule Morpheus do
   @moduledoc """
-  Documentation for `Morpheus`.
+  Morpheus is an Elixir library for converting between camelCase and snake_case in Phoenix projects.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Morpheus.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def camel_to_snake(string) when is_binary(string) do
+    string
+    |> String.replace(~r/([A-Z])/, "_\\1")
+    |> String.downcase()
+    |> String.trim_leading("_")
   end
+
+  def snake_to_camel(string) when is_binary(string) do
+    string
+    |> String.split("_")
+    |> Enum.map_join(&String.capitalize/1)
+    |> downcase_first()
+  end
+
+  defp downcase_first(<<first::utf8, rest::binary>>), do: String.downcase(<<first>>) <> rest
 end
