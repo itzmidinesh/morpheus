16ad021b6c5f1944eb8b8fb40e143454422425c2
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-10-13 21:58:53 +0530

    chore: update mix.exs with package details and dependency
    - Added package description and metadata, including maintainers, authors and links to GitHub repo.
    - Included the `jason` dependency for JSON encoding support.
    - Updated package information to include license and additional files for packaging.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Morpheus.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -21,8 +23,23 @@ defmodule Morpheus.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    "Morpheus is an Elixir library for converting between camelCase and snake_case in Phoenix projects."
+  end
+
+  defp package do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/itzmidinesh/morpheus"},
+      maintainers: ["Dinesh Anbazhagan"],
+      authors: ["Dinesh Anbazhagan"]
     ]
   end
 end
