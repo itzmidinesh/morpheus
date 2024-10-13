4ddeb2631c445b5995e86f70f086d01bc29e103a
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-10-13 21:31:07 +0530

    feat: enhance Morpheus module to improve handling and conversion
    - Added support for converting atoms to camelCase and snake_case in `camel_to_snake/1` and `snake_to_camel/1`.
    - Introduced generic conversion functions for both atoms and binaries, handling cases where the input is neither.
    - Improved `convert_map_keys/2` to support atom and binary keys, ensuring keys are properly converted in nested maps and lists.
    - Refactored the key conversion logic into a helper function `convert_key/2` to streamline the code.

--- lib/morpheus.ex
+++ lib/morpheus.ex
@@ -10,6 +10,15 @@ defmodule Morpheus do
     |> String.trim_leading("_")
   end
 
+  def camel_to_snake(atom) when is_atom(atom) do
+    atom
+    |> Atom.to_string()
+    |> camel_to_snake()
+    |> String.to_atom()
+  end
+
+  def camel_to_snake(input), do: input
+
   def snake_to_camel(string) when is_binary(string) do
     string
     |> String.split("_")
@@ -17,12 +26,21 @@ defmodule Morpheus do
     |> downcase_first()
   end
 
+  def snake_to_camel(atom) when is_atom(atom) do
+    atom
+    |> Atom.to_string()
+    |> snake_to_camel()
+    |> String.to_atom()
+  end
+
+  def snake_to_camel(input), do: input
+
   defp downcase_first(<<first::utf8, rest::binary>>), do: String.downcase(<<first>>) <> rest
 
   def convert_map_keys(map, conversion_function) when is_map(map) do
     map
     |> Enum.map(fn {key, value} ->
-      {conversion_function.(key), convert_map_keys(value, conversion_function)}
+      {convert_key(key, conversion_function), convert_map_keys(value, conversion_function)}
     end)
     |> Enum.into(%{})
   end
@@ -32,4 +50,10 @@ defmodule Morpheus do
   end
 
   def convert_map_keys(value, _conversion_function), do: value
+
+  defp convert_key(key, conversion_function) when is_atom(key) or is_binary(key) do
+    conversion_function.(key)
+  end
+
+  defp convert_key(key, _conversion_function), do: key
 end
