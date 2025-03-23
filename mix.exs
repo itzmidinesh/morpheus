0e5317804a01e272dae94d8538c8272a6b76b512
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2025-03-23 22:57:29 +0530

    chore: bump version to 0.1.1
    Update version to `0.1.1` for release.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Morpheus.MixProject do
   def project do
     [
       app: :morpheus,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
